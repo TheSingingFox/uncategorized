cf35e803a0cfe074a609ce69a5d0e0fcef4798e4
TheSingingFox <singingfox123@gmail.com>
2020-07-06 14:33:23 +0200

    improved documentation

--- test.cpp
+++ test.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
+//include the header file
 #include"psswd.h"
 using namespace std;
 int main(){
+	//call the function and put its output into a boolean variable
 	bool test = psswd();
+	//act upon the return value
 	if(test){cout << "You made it!\n";}
 	else{cout << "You're a failure!\n";}
 	return 0;}
