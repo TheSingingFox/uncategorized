f25f9badec5a2805d5ee5f92e3c152315dad3dc6
TheSingingFox <singingfox123@gmail.com>
2020-07-30 22:35:33 +0200

    made it into a calculator

--- base13/base13.c
+++ base13/base13.c
@@ -2,11 +2,36 @@
 #include<stdlib.h>
 
 int main() {
+  int input;
+  int input2;
+  char operation;
   int num;
   int solution;
 
-  printf("Enter a value: ");
-  scanf("%d", &num);
+  printf("Do you want to convert a number (=) or add (+), subtract (-), multiply (*) or divide (/) two?\n");
+  scanf("%c", &operation);
+
+  if(operation == '=') {
+    printf("Enter a value: ");
+    scanf("%d", &input);
+    num = input;
+  } else {
+    printf("Enter the first value: ");
+    scanf("%d", &input);
+    printf("Enter the second value: ");
+    scanf("%d", &input2);
+    if(operation == '+') {
+      num = input + input2;
+    } else if(operation == '-') {
+      num = input - input2;
+    } else if(operation == '*') {
+      num = input * input2;
+    } else if(operation == '/') {
+      num = input / input2;
+    }
+  }
+      
+
   solution = (num / 13) * 10 + num % 13;
 
   printf("%d\n", solution);
